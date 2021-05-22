96120c6e1b43bcd8133d9939bfcecd51d915e217
DxHeNT <wujiecao547498787@gmail.com>
2021-05-22 15:03:23 +0800

    chore: add normalize.css

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import router from './router';
 import store from './store';
 import { useElementPlus } from './plugins/element-plus';
 
+// normalize css
+import 'normalize.css';
+
 const app = createApp(App);
 
 app.use(router).use(store).use(useElementPlus).mount('#app');
