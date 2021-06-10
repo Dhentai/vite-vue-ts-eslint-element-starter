dc623bf0591a81a0ed6a12d39cc82e191313a5e3
DxHeNT <wujiecao547498787@gmail.com>
2021-06-10 15:15:12 +0800

    chore: add dark mode plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    plugins: [
-      require('autoprefixer'),
-      require('postcss-nesting')
-    ]
-  }
+  plugins: [
+    require('postcss-dark-theme-class'),
+    require('autoprefixer'),
+    require('postcss-nesting'),
+  ],
+};
